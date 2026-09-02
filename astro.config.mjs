36bbc03071f742a2068b753a5d8cc9cf68eae737
Dar Sissau <dar.sissau@student.howest.be>
2026-09-02 22:06:33 +0200

    privacy page and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // De 'base' moet exact overeenkomen met je GitHub Pages submap
 export default defineConfig({
   site: 'https://toneeldevalk.be', // Pas dit aan naar je uiteindelijke domein
+  integrations: [sitemap({
+    filter: (page) => !page.includes('/welovedevalk4ever'),
+  })],
   vite: {
     plugins: [tailwindcss()]
   }
